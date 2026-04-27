3cc3268366ef08fa0b669fa1d5aeabaa4ea5c682
berlus10 <djongonberlus@gmail.com>
2026-04-27 14:09:48 +0100

    feat: ajout du front

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Gestion des notes de frais",
+  description: "Remplissez facilement votre note de frais en quelques étapes. Téléchargez vos justificatifs et suivez vos remboursements.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
